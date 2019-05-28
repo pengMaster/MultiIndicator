9423bb2118b0fbf3014179b8a616d381483e910c
“pengMaster”  <946549990@qq.com>
2019-05-28 16:30:17 +0800

    add_tabView

--- app/src/main/java/com/example/multiindicator/TabViewAct.java
+++ app/src/main/java/com/example/multiindicator/TabViewAct.java
@@ -3,7 +3,13 @@ package com.example.multiindicator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.indicatorlib.base.BaseFragmentAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <pre>
@@ -20,14 +26,19 @@ public class TabViewAct extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_tab_view);
-
         initViewPager();
     }
 
     private void initViewPager() {
-        Fragment fragment1 = new Fragment();
-        Fragment fragment2 = new Fragment();
-
-        BaseF
+        List<Fragment> lists = new ArrayList<>();
+        Fragment fragment1 = new TestFragment();
+        Fragment fragment2 = new TestFragment();
+        lists.add(fragment1);
+        lists.add(fragment2);
+        BaseFragmentAdapter adapter = new BaseFragmentAdapter(getSupportFragmentManager(),lists);
+        ViewPager vpPersonManager = findViewById(R.id.vpPersonManager);
+        TabView tabView = findViewById(R.id.mIndicator);
+        vpPersonManager.setAdapter(adapter);
+        tabView.setViewPager(vpPersonManager);
     }
 }
---
+++ app/src/main/java/com/example/multiindicator/TestFragment.java
@@ -0,0 +1,27 @@
+package com.example.multiindicator;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * <pre>
+ *     author : Wp
+ *     e-mail : 1101313414@qq.com
+ *     time   : 2019/5/28 4:22 PM
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class TestFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_test,null);
+    }
+}
